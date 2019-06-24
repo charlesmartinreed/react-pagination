5c177f568c5749463768421475fa91c8a0433ca8
charlesmartinreed <charlesmartinreed@icloud.com>
2019-06-24 13:34:47 -0500

    create pagination component. create paginate method.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 import "./App.css";
 
 // on component mount, fetch posts
@@ -8,7 +9,7 @@ const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -24,11 +25,24 @@ const App = () => {
     fetchPosts();
   }, []);
 
-  console.log(posts);
+  // this will get the "current" posts
+  // remember that slice creates a shallow copy
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // change the page - pageNumber is passed by the pagination component
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">Latest Posts</h1>
-      <Posts posts={posts} loading={loading} />
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
